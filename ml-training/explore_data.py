4dd408b2fd57e6efc61b542419d7bc8ac02e1b00
Brian Park <brianpark1208@gmail.com>
2025-12-07 16:46:25 -0800

    delete ml_training folder

--- ml-training/explore_data.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-import os
-
-image_path = "path/to/falcons/image1.jpg"
-img = Image.open(image_path)
-print(f"Size: {img.size}")
-print(f"Format: {img.format}")
-print(f"Mode: {img.mode}")
-img.show()
